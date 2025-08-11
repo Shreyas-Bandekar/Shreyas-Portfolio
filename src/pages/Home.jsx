931f81dd31782c472dbe5950fd87d47013449ef3
Shreyas <shreyasbandekar01@gmail.com>
2025-08-11 09:46:29 +0530

    feat: Add react-helmet for improved SEO and update dependencies in package.json and package-lock.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,16 @@ import { SkillsSection } from '../components/SkillSection';
 import { ProjectSection } from '../components/ProjectSection';
 import { ContactSection } from '../components/ContactSection';
 import { FooterSection } from '../components/FooterSection';
+import  Helmet  from "react-helmet";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
-        
+        <Helmet>
+        <title>Shreyas Portfolio - Web Developer & AI/ML Enthusiast</title>
+        <meta name="description" content="Welcome to my portfolio where I showcase my projects in web development and AI/ML." />
+      </Helmet>
 
         {/* Background Effect */}
         <StarBg />
